4779a1c83ee3dffb0670c60a5340d2b47e8e75fe
Miguel <gannasong@gmail.com>
2021-08-30 00:09:15 +0800

    test: did start loading displays no errors and starts loading

--- CathayNowPlaying/CathayNowPlayingTests/Presentation/NowPlayingPresenterTests.swift
+++ CathayNowPlaying/CathayNowPlayingTests/Presentation/NowPlayingPresenterTests.swift
@@ -6,9 +6,45 @@
 //
 
 import XCTest
+import CathayNowPlaying
+
+struct NowPlayingLoadingViewModel: Equatable {
+  let isLoading: Bool
+}
+
+struct NowPlayingErrorViewModel: Equatable {
+  let message: String?
+
+  static var noError: NowPlayingErrorViewModel {
+    return NowPlayingErrorViewModel(message: nil)
+  }
+
+  static func error(message: String) -> NowPlayingErrorViewModel {
+    return NowPlayingErrorViewModel(message: message)
+  }
+}
+
+protocol NowPlayingLoadingView {
+  func display(_ viewModel: NowPlayingLoadingViewModel)
+}
+
+protocol NowPlayingErrorView {
+  func display(_ viewModel: NowPlayingErrorViewModel)
+}
 
 class NowPlayingPresenter {
+  private let loadingView: NowPlayingLoadingView
+  private let errorView: NowPlayingErrorView
 
+  init(loadingView: NowPlayingLoadingView, errorView: NowPlayingErrorView) {
+    self.loadingView = loadingView
+    self.errorView = errorView
+  }
+
+  func didStartLoading() {
+    loadingView.display(.init(isLoading: true))
+    errorView.display(.noError)
+  }
 }
 
 class NowPlayingPresenterTests: XCTestCase {
@@ -16,21 +52,42 @@ class NowPlayingPresenterTests: XCTestCase {
   func test_init_doesNotMessageView() {
     let (_, view) = makeSUT()
 
-    XCTAssertTrue(view.message.isEmpty)
+    XCTAssertTrue(view.messages.isEmpty)
+  }
+
+  func test_didStartLoading_displaysNoErrorsAndStartsLoading() {
+    let (sut, view) = makeSUT()
+
+    sut.didStartLoading()
+
+    XCTAssertEqual(view.messages, [.display(isLoading: true), .display(message: nil)])
   }
 
   // MARK: - Helpers
 
   func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: NowPlayingPresenter, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = NowPlayingPresenter()
+    let sut = NowPlayingPresenter(loadingView: view, errorView: view)
 
     trackForMemoryLeaks(view, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
   }
 
-  class ViewSpy {
-    private(set) var message: [Any] = []
+  class ViewSpy: NowPlayingLoadingView, NowPlayingErrorView {
+    enum Message: Equatable {
+      case display(isLoading: Bool)
+      case display(message: String?)
+    }
+
+    private(set) var messages: [Message] = []
+
+    func display(_ viewModel: NowPlayingLoadingViewModel) {
+      messages.append(.display(isLoading: viewModel.isLoading))
+    }
+
+    func display(_ viewModel: NowPlayingErrorViewModel) {
+      messages.append(.display(message: viewModel.message))
+    }
   }
 }
