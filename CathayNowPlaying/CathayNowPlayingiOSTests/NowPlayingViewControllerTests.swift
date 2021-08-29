594e933e4364b877412a7772b4d3bfa02fd81713
Miguel <gannasong@gmail.com>
2021-08-29 17:35:57 +0800

    test: on pull to refresh requests load

--- CathayNowPlaying/CathayNowPlayingiOSTests/NowPlayingViewControllerTests.swift
+++ CathayNowPlaying/CathayNowPlayingiOSTests/NowPlayingViewControllerTests.swift
@@ -10,6 +10,19 @@ import UIKit
 import CathayNowPlaying
 
 final class NowPlayingViewController: UIViewController {
+  private(set) lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+    collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    collectionView.backgroundColor = .systemBackground
+    collectionView.delegate = self
+
+    let refreshControl = UIRefreshControl(frame: .zero)
+    refreshControl.addTarget(self, action: #selector(load), for: .valueChanged)
+
+    collectionView.refreshControl = refreshControl
+    return collectionView
+  }()
+
   private var loader: NowPlayingLoader?
 
   convenience init(loader: NowPlayingLoader) {
@@ -19,22 +32,34 @@ final class NowPlayingViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    view.addSubview(collectionView)
     load()
   }
 
-  func load() {
+  @objc func load() {
     loader?.execute(PagedNowPlayingRequest(page: 1), completion: { _ in })
+  }
+}
+
+extension NowPlayingViewController: UICollectionViewDelegateFlowLayout {
+  func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    guard collectionView.refreshControl?.isRefreshing == true else { return }
+    load()
   }
 }
 
 class NowPlayingViewControllerTests: XCTestCase {
 
   func test_loadActions_requestNowPlayingFromLoader() {
+    let request: LoaderSpy.Message = .load(PagedNowPlayingRequest(page: 1))
     let (sut, loader) = makeSUT()
     XCTAssertTrue(loader.messages.isEmpty)
 
     sut.loadViewIfNeeded()
-    XCTAssertEqual(loader.messages, [.load(PagedNowPlayingRequest(page: 1))])
+    XCTAssertEqual(loader.messages, [request])
+
+    sut.simulateUserRefresh()
+    XCTAssertEqual(loader.messages, [request, request])
   }
 
   // MARK: - Helpers
@@ -58,5 +83,26 @@ class NowPlayingViewControllerTests: XCTestCase {
     func execute(_ request: PagedNowPlayingRequest, completion: @escaping (NowPlayingLoader.Result) -> Void) {
       messages.append(.load(request))
     }
+  }
+}
+
+extension NowPlayingViewController {
+  func simulateUserRefresh() {
+    collectionView.refreshControl?.beginRefreshing()
+    collectionView.refreshControl?.simulatePullToRefresh()
+  }
+}
+
+extension UIControl {
+  func simulate(event: UIControl.Event) {
+    allTargets.forEach { target in
+      actions(forTarget: target, forControlEvent: event)?.forEach { (target as NSObject).perform(Selector($0)) }
+    }
+  }
+}
+
+extension UIRefreshControl {
+  func simulatePullToRefresh() {
+    simulate(event: .valueChanged)
   }
 }
