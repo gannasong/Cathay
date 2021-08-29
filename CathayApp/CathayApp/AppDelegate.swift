27cb87ddcd43e03f7b62ed8dffa5a9c592f14776
Miguel <gannasong@gmail.com>
2021-08-29 14:52:58 +0800

    chore: create CathayApp iOS project

---
+++ CathayApp/CathayApp/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  CathayApp
+//
+//  Created by SUNG HAO LIN on 2021/8/29.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate { }
---
+++ CathayApp/CathayApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CathayApp
+//
+//  Created by SUNG HAO LIN on 2021/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
