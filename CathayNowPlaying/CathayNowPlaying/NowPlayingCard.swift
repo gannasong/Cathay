514392f6b705411f66e554934cc392ee0403b3c0
Miguel <gannasong@gmail.com>
2021-08-29 12:55:12 +0800

    feat: model NowPlayingCard

---
+++ CathayNowPlaying/CathayNowPlaying/NowPlayingCard.swift
@@ -0,0 +1,14 @@
+//
+//  NowPlayingCard.swift
+//  CathayNowPlaying
+//
+//  Created by SUNG HAO LIN on 2021/8/29.
+//
+
+import Foundation
+
+struct NowPlayingCard {
+  let id: Int
+  let title: String
+  let imagePath: String
+}
