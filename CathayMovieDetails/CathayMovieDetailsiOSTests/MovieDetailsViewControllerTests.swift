ad696474801fd2f55a02ad1fd88eb6932933520d
Miguel <gannasong@gmail.com>
2021-08-30 17:26:10 +0800

    test: request movie on view did load

--- CathayMovieDetails/CathayMovieDetailsiOSTests/MovieDetailsViewControllerTests.swift
+++ CathayMovieDetails/CathayMovieDetailsiOSTests/MovieDetailsViewControllerTests.swift
@@ -6,31 +6,60 @@
 //
 
 import XCTest
+import UIKit
+import CathayMovieDetails
 
-final class MovieDetailsViewController {
+final class MovieDetailsViewController: UIViewController {
+  private var id: Int?
+  private var loader: MovieLoader?
 
+  convenience init(id: Int, loader: MovieLoader) {
+    self.init(nibName: nil, bundle: nil)
+    self.id = id
+    self.loader = loader
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loader?.load(id: id!, completion: { _ in })
+  }
 }
 
 class MovieDetailsViewControllerTests: XCTestCase {
 
   func test_load_actionsRequestDetailsFromLoader() {
-    let (_, loader) = makeSUT()
+    let movieID = 100
+    let (sut, loader) = makeSUT(id: movieID)
 
     XCTAssertTrue(loader.messages.isEmpty)
+
+    sut.loadViewIfNeeded()
+    XCTAssertEqual(loader.messages, [.load(movieID)])
   }
 
   // MARK: - Helpers
 
-  func makeSUT(file: StaticString = #file, line: UInt = #line) -> (MovieDetailsViewController, LoaderSpy) {
+  func makeSUT(id: Int = 0, file: StaticString = #file, line: UInt = #line) -> (MovieDetailsViewController, LoaderSpy) {
     let loader = LoaderSpy()
-    let sut = MovieDetailsViewController()
+    let sut = MovieDetailsViewController(id: id, loader: loader)
     trackForMemoryLeaks(loader, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
 
     return (sut, loader)
   }
 
-  class LoaderSpy {
-    private(set) var messages: [Any] = []
+  class LoaderSpy: MovieLoader {
+
+    enum Message: Equatable {
+      case load(Int)
+    }
+
+    private(set) var messages: [Message] = []
+
+    typealias Result = MovieLoader.Result
+
+    func load(id: Int, completion: @escaping (Result) -> Void) {
+      messages.append(.load(id))
+    }
   }
 }
