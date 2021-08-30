20a325c296e5441c05bb14e4bd9b38b95f05ecbe
Miguel <gannasong@gmail.com>
2021-08-30 23:31:40 +0800

    refactor: move MovieLoaderPresentationAdapter to own group

--- CathayMovieDetails/CathayMovieDetailsiOS/Composers/MovieDetailsUIComposer.swift
+++ CathayMovieDetails/CathayMovieDetailsiOS/Composers/MovieDetailsUIComposer.swift
@@ -11,6 +11,7 @@ import CathayCommon
 import CathayMovieDetails
 
 public final class MovieDetailsUIComposer {
+  
   private init() {}
 
   public static func compose(id: Int, loader: MovieLoader, imageLoader: ImageDataLoader, onPurchaseCallback: @escaping () -> Void) -> MovieDetailsViewController {
@@ -24,50 +25,6 @@ public final class MovieDetailsUIComposer {
     viewController.onBuyTicket = onPurchaseCallback
     adapter.presenter = MovieDetailsPresenter(view: WeakRefVirtualProxy(viewController), imageTransformer: UIImage.init)
     return viewController
-  }
-}
-
-final class MovieLoaderPresentationAdapter<View: MovieDetailsView, Image>: MovieDetailsViewControllerDelegate where View.Image == Image {
-
-  var presenter: MovieDetailsPresenter<View, Image>?
-
-  private let id: Int
-  private let loader: MovieLoader
-  private let imageLoader: ImageDataLoader
-
-  private var task: ImageDataLoaderTask?
-
-  init(id: Int, loader: MovieLoader, imageLoader: ImageDataLoader) {
-    self.id = id
-    self.loader = loader
-    self.imageLoader = imageLoader
-  }
-
-  func didRequestLoad() {
-    presenter?.didStartLoading()
-    loader.load(id: id, completion: { [weak self] result in
-      guard let self = self else { return }
-      switch result {
-      case let .success(movie): self.loadImage(for: movie)
-      case let .failure(error): self.presenter?.didFinishLoading(with: error)
-      }
-    })
-  }
-}
-
-private extension MovieLoaderPresentationAdapter {
-  func loadImage(for model: Movie) {
-    task = imageLoader.load(from: makeImageURL(withPath: model.backdropImagePath), completion: { [weak self] result in
-      guard let self = self else { return }
-      switch result {
-      case let .success(imageData): self.presenter?.didFinishLoadingImageData(with: imageData, for: model)
-      case let .failure(error): self.presenter?.didFinishLoadingImageData(with: error, for: model)
-      }
-    })
-  }
-
-  func makeImageURL(withPath path: String) -> URL {
-    return URL(string: "https://image.tmdb.org/t/p/w1280/")!.appendingPathComponent(path)
   }
 }
 
