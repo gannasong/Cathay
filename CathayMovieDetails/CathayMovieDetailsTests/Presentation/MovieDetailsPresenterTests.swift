9b45a7a35e91e46f139ef72f870cbbee9ae417b4
Miguel <gannasong@gmail.com>
2021-08-30 22:30:49 +0800

    test: on load movie success with failed image download renders details and stops loading

--- CathayMovieDetails/CathayMovieDetailsTests/Presentation/MovieDetailsPresenterTests.swift
+++ CathayMovieDetails/CathayMovieDetailsTests/Presentation/MovieDetailsPresenterTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import CathayMovieDetails
 
 struct MovieDetailsViewModel<Image> {
   let image: Image?
@@ -48,6 +49,41 @@ class MovieDetailsPresenter<View: MovieDetailsView, Image> where View.Image == I
 
   func didFinishLoading(with error: Error) {
     view.display(.showError(message: error.localizedDescription))
+  }
+
+  func didFinishLoadingImageData(with error: Error, for movie: Movie) {
+    view.display(MovieDetailsViewModel<Image>(image: nil,
+                                              title: movie.title,
+                                              meta: makeMovieMeta(length: movie.length, genres: movie.genres),
+                                              overview: movie.overview,
+                                              isLoading: false,
+                                              error: nil))
+  }
+
+  private func makeMovieMeta(length: CGFloat, genres: [String]) -> String {
+    let runTime = Double(length * 60).asString(style: .short)
+    let genres = genres.map { $0.capitalizingFirstLetter() }.joined(separator: ", ")
+    return "\(runTime) | \(genres)"
+  }
+}
+
+extension String {
+  func capitalizingFirstLetter() -> String {
+    return prefix(1).capitalized + dropFirst()
+  }
+
+  mutating func capitalizeFirstLetter() {
+    self = self.capitalizingFirstLetter()
+  }
+}
+
+extension Double {
+  func asString(style: DateComponentsFormatter.UnitsStyle) -> String {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.hour, .minute, .second]
+    formatter.unitsStyle = style
+    guard let formattedString = formatter.string(from: self) else { return "" }
+    return formattedString
   }
 }
 
@@ -92,6 +128,24 @@ class MovieDetailsPresenterTests: XCTestCase {
     XCTAssertNil(message?.image)
   }
 
+  func test_load_loadMovieSuccessWithFailedImageDownloadRendersDetailsAndStopsLoading() {
+    let (sut, view) = makeSUT()
+    let movie = makeMovie()
+    let error = makeError("uh oh, could not find image for movie")
+
+    sut.didFinishLoadingImageData(with: error, for: movie)
+
+    let message = view.messages.first
+    XCTAssertEqual(view.messages.count, 1)
+
+    XCTAssertEqual(message?.isLoading, false)
+    XCTAssertEqual(message?.title, movie.title)
+    XCTAssertEqual(message?.meta, "2 hr, 19 min | Action, Romance")
+    XCTAssertEqual(message?.overview, movie.overview)
+    XCTAssertNil(message?.image)
+    XCTAssertNil(message?.error)
+  }
+
   // MARK: - Helpers
 
   func makeSUT(imageTransformer: @escaping (Data) -> SomeImage? = { _ in nil }, file: StaticString = #file, line: UInt = #line) -> (sut: MovieDetailsPresenter<ViewSpy, SomeImage>, view: ViewSpy) {
@@ -101,6 +155,16 @@ class MovieDetailsPresenterTests: XCTestCase {
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
   }
+
+  func makeMovie() -> Movie {
+     return Movie(id: 1,
+                  title: "A movie",
+                  rating: 8.4,
+                  length: 139,
+                  genres: ["Action", "Romance"],
+                  overview: "A romantic action movie",
+                  backdropImagePath: "any.png")
+   }
 
   struct SomeImage: Equatable { }
 
