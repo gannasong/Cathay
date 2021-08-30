a44816869076aab1cf9bd359c72b9fdb216911ef
Miguel <gannasong@gmail.com>
2021-08-30 10:54:07 +0800

    test: did start loading image data and starts loading state

--- CathayNowPlaying/CathayNowPlayingTests/Presentation/NowPlayingImagePresenterTests.swift
+++ CathayNowPlaying/CathayNowPlayingTests/Presentation/NowPlayingImagePresenterTests.swift
@@ -6,9 +6,30 @@
 //
 
 import XCTest
+import CathayNowPlaying
 
-final class NowPlayingImagePresenter {
+struct NowPlayingImageViewModel<Image> {
+  let image: Image?
+  let title: String
+  let isLoading: Bool
+}
 
+protocol NowPlayingImageView {
+  associatedtype Image
+
+  func display(_ model: NowPlayingImageViewModel<Image>)
+}
+
+final class NowPlayingImagePresenter<View: NowPlayingImageView, Image> where View.Image == Image {
+  private let view: View
+
+  init(view: View) {
+    self.view = view
+  }
+
+  func didStartLoadingImageData(for model: NowPlayingCard) {
+    view.display(NowPlayingImageViewModel<Image>(image: nil, title: model.title, isLoading: true))
+  }
 }
 
 class NowPlayingImagePresenterTests: XCTestCase {
@@ -19,17 +40,40 @@ class NowPlayingImagePresenterTests: XCTestCase {
     XCTAssertTrue(view.messages.isEmpty)
   }
 
+  func test_load_didStartLoadingImageDataAndStartsLoadingState() {
+    let (sut, view) = makeSUT()
+    let item = makeNowPlayingCard(id: 123)
+
+    sut.didStartLoadingImageData(for: item)
+    let message = view.messages.first
+
+    XCTAssertEqual(view.messages.count, 1)
+    XCTAssertEqual(message?.isLoading, true)
+    XCTAssertEqual(message?.title, item.title)
+    XCTAssertNil(message?.image)
+  }
+
   // MARK: - Helpers
 
-  func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: NowPlayingImagePresenter, view: ViewSpy) {
+  func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: NowPlayingImagePresenter<ViewSpy, SomeImage>, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = NowPlayingImagePresenter()
+    let sut = NowPlayingImagePresenter(view: view)
     trackForMemoryLeaks(view, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
   }
 
-  class ViewSpy {
-    private(set) var messages: [Any] = []
+  func makeNowPlayingCard(id: Int) -> NowPlayingCard {
+    return NowPlayingCard(id: id, title: "Card #\(id)", imagePath: "image-for-card-\(id).png")
+  }
+
+  struct SomeImage: Equatable { }
+
+  class ViewSpy: NowPlayingImageView {
+    private(set) var messages: [NowPlayingImageViewModel<SomeImage>] = []
+
+    func display(_ model: NowPlayingImageViewModel<SomeImage>) {
+      messages.append(model)
+    }
   }
 }
