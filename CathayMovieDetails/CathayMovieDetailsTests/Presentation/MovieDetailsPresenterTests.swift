36a4a2db9952f990cb8b8398becf03cc9f24fff3
Miguel <gannasong@gmail.com>
2021-08-30 21:31:52 +0800

    test: on start loading hides error and starts loading state

--- CathayMovieDetails/CathayMovieDetailsTests/Presentation/MovieDetailsPresenterTests.swift
+++ CathayMovieDetails/CathayMovieDetailsTests/Presentation/MovieDetailsPresenterTests.swift
@@ -7,8 +7,39 @@
 
 import XCTest
 
-class MovieDetailsPresenter {
+struct MovieDetailsViewModel<Image> {
+  let image: Image?
+  let title: String?
+  let meta: String?
+  let overview: String?
 
+  let isLoading: Bool
+  let error: String?
+
+  static var showLoading: MovieDetailsViewModel<Image> {
+    return MovieDetailsViewModel<Image>(image: nil, title: nil, meta: nil, overview: nil, isLoading: true, error: nil)
+  }
+}
+
+protocol MovieDetailsView {
+  associatedtype Image
+  func display(_ model: MovieDetailsViewModel<Image>)
+}
+
+
+class MovieDetailsPresenter<View: MovieDetailsView, Image> where View.Image == Image {
+
+  private let view: View
+  private let imageTransformer: (Data) -> Image?
+
+  init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+    self.view = view
+    self.imageTransformer = imageTransformer
+  }
+
+  func didStartLoading() {
+    view.display(.showLoading)
+  }
 }
 
 class MovieDetailsPresenterTests: XCTestCase {
@@ -19,17 +50,39 @@ class MovieDetailsPresenterTests: XCTestCase {
     XCTAssertTrue(view.messages.isEmpty)
   }
 
+  func test_load_startLoadingHidesErrorAndStartsLoadingState() {
+    let (sut, view) = makeSUT()
+
+    sut.didStartLoading()
+
+    let message = view.messages.first
+    XCTAssertEqual(view.messages.count, 1)
+
+    XCTAssertEqual(message?.isLoading, true)
+    XCTAssertNil(message?.title)
+    XCTAssertNil(message?.meta)
+    XCTAssertNil(message?.overview)
+    XCTAssertNil(message?.error)
+    XCTAssertNil(message?.image)
+  }
+
   // MARK: - Helpers
 
-  func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: MovieDetailsPresenter, view: ViewSpy) {
+  func makeSUT(imageTransformer: @escaping (Data) -> SomeImage? = { _ in nil }, file: StaticString = #file, line: UInt = #line) -> (sut: MovieDetailsPresenter<ViewSpy, SomeImage>, view: ViewSpy) {
     let view = ViewSpy()
-    let sut = MovieDetailsPresenter()
+    let sut = MovieDetailsPresenter<ViewSpy, SomeImage>(view: view, imageTransformer: imageTransformer)
     trackForMemoryLeaks(view, file: file, line: line)
     trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, view)
   }
 
-  class ViewSpy {
-    private(set) var messages: [Any] = []
+  struct SomeImage: Equatable { }
+
+  class ViewSpy: MovieDetailsView {
+    private(set) var messages: [MovieDetailsViewModel<SomeImage>] = []
+
+    func display(_ model: MovieDetailsViewModel<SomeImage>) {
+      messages.append(model)
+    }
   }
 }
