817e02fec06a353ad21e245cdc71716de93cf213
Miguel <gannasong@gmail.com>
2021-08-30 18:18:30 +0800

    feat: on select now playing card push detail view

--- CathayApp/CathayApp/SceneDelegate.swift
+++ CathayApp/CathayApp/SceneDelegate.swift
@@ -10,17 +10,21 @@ import CathayNetworking
 import CathayMedia
 import CathayNowPlaying
 import CathayNowPlayingiOS
+import CathayMovieDetails
+import CathayMovieDetailsiOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   private lazy var config: AuthConfig = getConfig(fromPlist: "AuthConfig")
   private lazy var baseURL: URL = URL(string: "https://api.themoviedb.org")!
+  private lazy var navController = UINavigationController()
 
   var window: UIWindow?
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
-    window?.rootViewController = UINavigationController(rootViewController: makeNowPlayingScene())
+    navController.setViewControllers([makeNowPlayingScene()], animated: true)
+    window?.rootViewController = navController
     window?.makeKeyAndVisible()
   }
 
@@ -31,7 +35,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     let loader = RemoteNowPlayingLoader(baseURL: baseURL, client: authzClient)
     let imageLoader = RemoteImageDataLoader(client: client)
 
-    let viewController = NowPlayingUIComposer.compose(loader: loader, imageLoader: imageLoader, onSelectCallback: { _ in })
+    let viewController = NowPlayingUIComposer.compose(loader: loader, imageLoader: imageLoader) { [weak self] movieID in
+      guard let self = self else { return }
+
+      let detailsViewController = self.makeMovieDetailScene(for: movieID)
+      self.navController.pushViewController(detailsViewController, animated: true)
+    }
+    return viewController
+  }
+
+  private func makeMovieDetailScene(for id: Int) -> MovieDetailsViewController {
+    let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
+    let authzClient = AuthenticatedHTTPClientDecorator(decoratee: client, config: config)
+
+    let loader = RemoteMovieLoader(baseURL: baseURL, client: authzClient)
+    let viewController = MovieDetailsUIComposer.compose(id: id, loader: loader, onPurchaseCallback: { })
     return viewController
   }
 }
