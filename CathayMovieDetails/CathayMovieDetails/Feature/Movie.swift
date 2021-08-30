a7959f4f89513e89d098ac74d9aabfbdb380a8ee
Miguel <gannasong@gmail.com>
2021-08-30 16:07:37 +0800

    feat: create Movie model

---
+++ CathayMovieDetails/CathayMovieDetails/Feature/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  CathayMovieDetails
+//
+//  Created by SUNG HAO LIN on 2021/8/30.
+//
+
+import Foundation
+
+struct Movie {
+  let id: String
+  let title: String
+  let rating: Int
+  let runTime: Int
+  let genres: [String]
+  let overview: String
+  let backdropImagePath: String
+}
